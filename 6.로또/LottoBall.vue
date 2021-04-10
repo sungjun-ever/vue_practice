7830eced69a3c1e9765a721a38052514f0285ed4
sungjun-ever <zkfmak10@gmail.com>
2021-04-10 17:42:01 +0900

    6.로또

---
+++ 6.로또/LottoBall.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="ball" :style="{ background }">{{number}}</div>
+</template>
+
+<script>
+    export default {
+        name: 'LotooBall',
+        props: {
+            number: Number,
+        },
+
+        computed: {
+            background() {
+                let background;
+                if (this.number < 10) {
+                    background = 'red';
+                } else if (this.number <= 20) {
+                    background = 'orange';
+                } else if (this.number <= 30) {
+                    background = 'yellow';
+                } else if (this.number <= 40) {
+                    background = 'blue';
+                } else {
+                    background = 'green';
+                }
+                return background;
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .ball {
+        display: inline-block;
+        border: 1px solid black;
+        border-radius: 20px;
+        width: 40px;
+        height: 40px;
+        line-height: 40px;
+        font-size: 20px;
+        text-align: center;
+        margin-right: 20px;
+    }
+</style>
